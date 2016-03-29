c51ea9c6c179ca1d67d0a2bd038c0e22f29127ef
owen <stensonowen@gmail.com>
2016-03-29 17:06:07 -0400

    file input

--- ds_hw/hw1_f09.rs
+++ ds_hw/hw1_f09.rs
@@ -4,8 +4,9 @@ use std::env;
 //use std::io::File;
 use std::error::Error;
 use std::fs::File;
-use std::io::prelude::*;
 use std::path::Path;
+use std::io::BufReader;
+use std::io::BufRead;
 
 enum Align { 
     Left, 
@@ -16,39 +17,35 @@ enum Align {
 fn main() {
     //Arg parsing:
     let args: Vec<String> = env::args().collect();
-    //if env::args().len() != 5 { 
     if args.len() != 5 { 
         panic!("Usage: ./a.out input output width alignment"); 
     }
-    //let align = match env::args().nth(4){
     let align = match args[4].as_ref(){
-        "flush_left" => Align::Left,
-        //Some(ref x) if x == "flush_left"    => Align::Left,
-        //Some(ref x) if x == "flush_right"   => Align::Right,
-        //Some(ref x) if x == "full_justify"  => Align::Full,
+        "flush_left"    => Align::Left,
+        "flush_right"   => Align::Right,
+        "full_justify"  => Align::Full,
         _   => panic!("Alignment must be 'flush_left', 'flush_right', or 'full_justify'"), 
     };
-    //let fn_in = env::args().nth(1);
-    //let fn_out= env::args().nth(2);
     let fn_in = &args[1];
     let fn_out= &args[2];
-    //let width: u32 = match env::args().nth(3).map(|a| a.parse()) {
-    //    Some(Ok(n)) => n,
-    //    _   => panic!("Width must be an unsigned integer"),
-    //};
     let width: u32 = match args[3].trim().parse(){
-    //let width: u32 = match args[3].parse().unwrap(){
         Ok(n) => n,
-        _   => panic!("SDF"),
+        _   => panic!("Width must be a positive integer"),
     };
-    println!("Width = {}", width);
-    //
+
     ////file io
-    //let path = Path::new("tmp.txt");
-    //let mut f_in = match File::open(&path) {
-    //    Err(e) => panic!("failed to open {}: {}", path.display(), Error::description(&e)),
-    //    Ok(f)  => f,
+    let f_in = match File::open(fn_in) {
+        Err(e) => panic!("Failed to open input file {}: {}", fn_in, Error::description(&e)),
+        Ok(f)  => f,
+    };
+    let f_in_reader = BufReader::new(f_in);
+    for line in f_in_reader.lines(){
+        println!("{}", line.unwrap());
+    }
+    let f_out= Path::new(fn_out);
+    //let mut f_out = match File::open(&f_out) {
+    //    Err(e)  => panic!("Failed to open output file {}: {}", f_out.display(), Error::description(&e)),
+    //    Ok(f)   => f,
     //};
-    //let tmp : String = fn_in;
 
 }
