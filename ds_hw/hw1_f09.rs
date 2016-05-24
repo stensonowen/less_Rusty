6073d8767943d731c489e6aa45c24d514204974a
owen <stensonowen@gmail.com>
2016-05-23 23:58:12 -0400

    think I forgot to commit this; will be used as a starting point as I resume work on this project

--- ds_hw/hw1_f09.rs
+++ ds_hw/hw1_f09.rs
@@ -13,85 +13,13 @@ enum Align {
     Full,
 }
 
-fn format<'a>(mut lines: &'a Vec<String>, align: Align, width: usize) -> &'a Vec<String> {
-    for line in lines {
-        //line will be string of words separated by spaces
-        if align == Align::Full {
-            let v : Vec<_> = line.split(' ').collect();
-            let num_letters = line.len() - match v.len() {
-                //number of spaces should be excluded from count of current real letters:
-                0|1 => 0,
-                x   => x-1,
-            };
-            //let tmp = v.iter().fold(String::new(), |l, w| l.push_str(w));
-            let mut spaces = String::new();
-            for i in 0..(width-num_letters) {
-                spaces.push(' ');
-            }
-            //let line = &v.connect(&spaces);
-            //let line = "foo";
-            let line = "foo".to_string();
-        }
-        for l in lines {
-            println!(":{}", l);
-        }
-        let spaces = match align {
-            //align = l/r justify: => # spaces before/after
-            //align = c justify:   => # spaces in between
-            Align::Left | Align::Right => width - line.len(),
-            //Align::Cent 
-           _    => 5, 
-       };
-    }
-    &lines
-}
-
-fn split_up(f_in: &File, width: usize) -> Vec<String> {
-    //prepare vector that is to hold formatted lines
-    let mut lines = Vec::<String>::new();
-    lines.push(String::new());
-    let mut l = 0;  //current line to alter
-    //prepare to read from input one line at a time
-    //dealing with one line at a time is preferable to reading 
-    // into a single string and then tokenizing it all
-    let f_in_reader = BufReader::new(f_in);
-    for line in f_in_reader.lines(){
-        //split line into words and cycle through them
-        let line = line.unwrap();
-        let words = line.split(' ');
-        for word in words {
-            //not sophisticated enough to split individual words by syllable
-            // no hyphenation -> undefined case: throw error
-            if word.len() > width {
-                panic!("Error: word <{}> in line <{}> is longer than the desired width: {} > {}",
-                       word, line, word.len(), width);
-            }
-            //append word to formatted line if it won't put it over the width threshold
-            else if lines[l].len() + word.len() < width {
-                lines[l].push(' ');
-                lines[l].push_str(word);
-            } 
-            //otherwise push it to the next line
-            else {
-                lines.push(word.to_string());
-                l += 1;
-            }
-        }
-    }
-    /*for line in &lines {
-        println!("{}", line);
-    }*/
-    lines
-}
-
-fn write_out(mut f_out: &File, lines: &Vec<String>) {
+fn write_out(mut f_out: &File, lines: &Vec<&str>) {
     let lorem_ipsum = "LOREM IPSUM\n".to_string();
     if let Err(e) = f_out.write_all(lorem_ipsum.as_bytes()){
         //don't want to use a match statement,
         // because Ok(_) should do nothing
         println!("failed to write to output file: {}", Error::description(&e));
     }
-    
 }
 
 fn main() {
@@ -124,9 +52,91 @@ fn main() {
         Ok(f)   => f,
     };
 
-    //puttin' in work
-    let mut lines = split_up(&f_in, width);
-    lines = format(&lines, align, width);
-    write_out(&f_out, &lines);
+    let words = tokenize(&f_in);
+    let lines = split(&words, width);
+    for line in &lines{
+        for word in line{
+            print!("{}, ", word);
+        }
+        println!("");
+    }
+    println!("{}", lines.len());
+}
+
+fn format(lines: &Vec<Vec<&String>>, align: Align, width: usize) -> Vec<String> {
+    let mut output = Vec::<String>::new();
+    output.push(String::new());
+    for line in lines {
+        //len = length of all the words in the line (excluding spaces)
+        let len = line.iter().fold(0, |acc, &l| acc+l.len());
+        let num_spaces = match line.len(){
+            0|1 => 1,
+            l   => l-1,
+        };
+        let delim_len = match align {
+            Align::Left | Align::Right => 1,
+            Align::Full => (width-len)/num_spaces,
+        };
+        let l_buffer = match align {
+            Align::Right => width-(len+num_spaces),
+            _ => 0,
+        };
+        let r_buffer = width - (len + num_spaces*delim_len);
+        //let new_line = std::iter::repeat(' ').take(l_buffer).collect::<String>()
+        //             + line.collect(std::iter::repeat(' ').take(delim_len).collect::<String>())
+        //             + std::iter::repeat(' ').take(r_buffer).collect::<String>();
+        let delim = std::iter::repeat(' ').take(delim_len).collect::<String>();
+        let mut result = String::new();
+
+        result.push_str(std::iter::repeat(' ').take(l_buffer).collect::<String>().as_ref());
+
+        let mut tmp = String::new();
+        for w in line{  tmp.push_str(&delim); tmp.push_str(w);  }
+
+        let r = std::iter::repeat(' ').take(r_buffer).collect::<String>();
+
+        //line.as_slice().join("F");
+       // [line].to_vec().join(&&String::from("x"));
+        let mut x = line.clone();
+        x.join("F");
+        
+
+        output.push(result);
+    }
+    output
 
 }
+
+fn tokenize(f_in: &File) -> Vec<String>{
+    let mut words = Vec::new();
+    let reader = BufReader::new(f_in);
+    for line in reader.lines(){
+        for word in line.unwrap().split(' '){
+            //creates copy of each word
+            //could be done better using lifetimes?
+            words.push(word.to_string());
+        }
+    }
+    words
+}
+
+//fn split(words: &Vec<String>, width: usize) -> Vec<Vec<String>>{
+fn split(words: &Vec<String>, width: usize) -> Vec<Vec<&String>>{
+    let mut lines = Vec::new();
+    lines.push(Vec::new());
+    let mut line_len = 0;
+    let mut line_cnt = 1;
+    for word in words{
+        //lines[0].push(word.to_string());
+        if line_len + word.len() <= width {
+            line_len += word.len() + 1;
+        } else {
+            line_len = 0;
+            lines.push(Vec::new());
+            line_cnt += 1;
+        }
+        lines[line_cnt-1].push(word);
+    }
+    lines
+}
+
