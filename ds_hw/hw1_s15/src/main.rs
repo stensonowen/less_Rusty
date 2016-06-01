eaa41a02652f15604b55c2ad25ec28fce0dccea6
owen <stensonowen@gmail.com>
2016-06-01 19:46:55 -0400

    probably gotta fuck around with lifetimes again

--- ds_hw/hw1_s15/src/main.rs
+++ ds_hw/hw1_s15/src/main.rs
@@ -2,6 +2,69 @@
 
 extern crate clap;
 use clap::{App, Arg, SubCommand,};
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+//use std::io::Write;
+use std::error::Error;
+use std::path::Path;
+
+
+#[derive(Debug)]
+enum State {
+    Old(char),
+    New(char),
+}
+
+#[derive(Debug)]
+struct Board {
+    width:  usize,
+    height: usize,
+    board:  Vec<Vec<State>>,
+}
+
+impl Board {
+    fn new(f_in: &File) -> Board {
+        let mut board: Vec<Vec<State>> = vec![];
+        let mut char_line: Vec<State> = vec![];
+        let reader = BufReader::new(f_in);
+        for file_line in reader.lines() {
+            for c in file_line.unwrap().chars() {
+                //chars are smaller than u8s (by x2)
+                char_line.push(State::Old(c));
+            }
+            board.push(char_line);
+            char_line = vec![];
+        }
+        Board{ 
+            width:  board[0].len(),
+            height: board.len(),
+            board:  board 
+        }
+    }
+    fn print(&self) {
+        println!("Printing board; height={}, width={}", self.height, self.width);
+        for line in &self.board {
+            for c in line {
+                let c = match *c {
+                    State::New(c) => c,
+                    State::Old(c) => c,
+                };
+                print!("{}", c);
+            }
+            println!("");
+        }
+    }
+    fn submit(&self) {
+        for mut line in &self.board {
+            for mut c in line {
+                c = State::New('x');
+            }
+        }
+    }
+
+}
+
+
 
 #[allow(unused_variables)]
 fn main() {
@@ -31,8 +94,23 @@ fn main() {
                                 .arg(Arg::with_name("new").required(true)))
                     .get_matches();
     
-    let input  = matches.value_of("input");
-    let output = matches.value_of("output");
+    let input  = matches.value_of("input") .unwrap();
+    let output = matches.value_of("output").unwrap();
+	//file io
+    let f_in = match File::open(&input) {
+        Err(e) => panic!("Failed to open input file {}: {}", 
+                         input, Error::description(&e)),
+        Ok(f)  => f,
+    };
+    let fn_out= Path::new(&output);
+    let f_out = match File::create(&fn_out){
+        Err(e)  => panic!("failed to create file {}: {}", 
+                          fn_out.display(), Error::description(&e)),
+        Ok(f)   => f,
+    };
+
+    let board = Board::new(&f_in);
+    board.print();
     
     if let Some(m) = matches.subcommand_matches("replace"){
         if let (Some(new), Some(old)) = (m.value_of("new"), m.value_of("old")){
@@ -57,5 +135,8 @@ fn main() {
         println!("Commands include `replace`, `dilation`, `erosion`, and `floodfill`");
         std::process::exit(1);  //exit code clap uses
     }
+
+
+
 
 }
